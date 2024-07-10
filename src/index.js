a09ef2cd8a9422bed47bc893594df5b63e7a66db
marcellov7 <104211738+marcellov7@users.noreply.github.com>
2024-07-10 17:06:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -69,7 +69,11 @@ function TeyutoPlayer(posElem, obj) {
 		options.token = '';
 	}
 
-	let urlIframe = 'https://teyuto.tv/video/player?w=' + idVideo +'&cid=' + channel +'&token=' + options.token + '&auto=' + options.autoplay + '&muted=' + options.muted + '&controls=' + options.controls + '&playbackRates=' + options.playbackRates + '&qualitySelector=' + options.qualitySelector + '&playerColor=' + options.playerColor + '&loop=' + options.loop + '&captions=' + options.captions + '&seekButtons=' + options.seekButtons +'&lowLatency=' + options.lowLatency+'&related=' + options.related+'&relatedTags=' + options.relatedTags;
+	var teyutoPlayerDomain = 'https://teyuto.tv';
+	if(option.current_domain){
+	   teyutoPlayerDomain = '';
+	}
+	let urlIframe = teyutoPlayerDomain + '/video/player?w=' + idVideo +'&cid=' + channel +'&token=' + options.token + '&auto=' + options.autoplay + '&muted=' + options.muted + '&controls=' + options.controls + '&playbackRates=' + options.playbackRates + '&qualitySelector=' + options.qualitySelector + '&playerColor=' + options.playerColor + '&loop=' + options.loop + '&captions=' + options.captions + '&seekButtons=' + options.seekButtons +'&lowLatency=' + options.lowLatency+'&related=' + options.related+'&relatedTags=' + options.relatedTags;
 
 	if (options.responsive != 'on') {
 		videoframe = '<iframe id="iframePlayerTeyuto-' + uniqueVal + '" width="' + options.width + '" height="' + options.height + '" src="' + urlIframe + '" frameborder="0" allow="autoplay" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>';
